5abf714401be933e570a676bdcbc293b6363a34e
Galina Kochneva <checkbox88@gmail.com>
2018-03-09 17:45:21 -0800

    PointTests

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void testDistance() {
+
+        Point p1 = new Point(5, 15);
+        Point p2 = new Point(5, 10);
+        Assert.assertEquals(Point.distance(p1, p2), 5.0);
+    }
+}
